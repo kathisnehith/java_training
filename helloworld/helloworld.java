8a9eeec0f083423c55e53f293ec8240d70650408
Snehith Reddy Kathi <kathisnehithreddy@gmail.com>
2025-07-19 16:04:56 -0500

    first code

--- helloworld/helloworld.java
+++ helloworld/helloworld.java
@@ -1,6 +1,5 @@
-package helloworld;
-public class helloworld {
-    public static void main(String args[]){
+public class HelloWorld{
+    public static void main(String[] args){
         System.out.println("Hello world");
     }
 }
